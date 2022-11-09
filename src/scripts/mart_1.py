929eff97785bd74567830d8642df816249053e71
22Vadim <105052322+22Vadim@users.noreply.github.com>
2022-11-09 16:03:11 +0300

    Update mart_1.py

--- src/scripts/mart_1.py
+++ src/scripts/mart_1.py
@@ -17,71 +17,78 @@ def event_with_city(geo_events_source: str, geo_cities: str, spark: SparkSession
 
     events = spark.read.parquet(geo_events_source)
 
-    events_day = events.filter(F.col("lat").isNotNull() & F.col("lon").isNotNull() & (events.event_type == "message"))
+    events_day = events.filter(F.col("lat").isNotNull() & F.col("lon").isNotNull() & 
+                           (events.event_type == "message")).where("date < '2022-03-30'")  
 
-    event_day =  events_day.select(F.col('event.message_from'), 
-                                F.col('event.datetime'),
-                                F.col('event.message_id'), 
-                                F.col('date'), 
-                                F.col('lat'),  
-                                F.col('lon'))
+    events_day =  events_day.select(F.col('event.message_from'), 
+                               F.col('event.datetime'),
+                               F.col('event.message_id'), 
+                               F.col('date'), 
+                               F.col('lat'),  
+                               F.col('lon'))
 
-    geo = spark.read.csv("/user/vsmirnov22/data/geo.csv", sep =';', header = True)
+    geo = spark.read.csv(geo_cities, sep =';', header = True)
 
     geo = geo.withColumn('lat', F.regexp_replace('lat', ',', '.').cast('double')) \
                      .withColumn('lng', F.regexp_replace('lng', ',', '.').cast('double')) \
                      .select( F.col('city') ,F.col('lat').alias('lat_2'), F.col('lng').alias('lon_2'))
 
-    new = event_day.crossJoin(geo)
+    nnew = events_day.crossJoin(geo)
 
     new = new.withColumn( 'km' , 2 * 6371 * F.asin(F.sqrt(F.sin(((F.radians(F.col("lat_2"))) - (F.radians(F.col("lat")))) / 2)**2  
-                                                        + F.cos((F.radians(F.col("lat"))))*F.cos((F.radians(F.col("lat_2"))))
-                                                        *F.sin(((F.radians(F.col("lon_2"))) - (F.radians(F.col("lon"))))/2)**2)))
+                                                      + F.cos((F.radians(F.col("lat"))))*F.cos((F.radians(F.col("lat_2"))))
+                                                      *F.sin(((F.radians(F.col("lon_2"))) - (F.radians(F.col("lon"))))/2)**2)))
 
 
     window = Window().partitionBy('message_from', "message_id")
 
     new_2 = new.withColumn("min_km", F.min('km').over(window)) \
-                        .select('message_from', 'datetime', 'message_id', 'date', 'city', 'km', 'min_km')
+                    .select('message_from', 'datetime', 'message_id', 'date', 'city', 'lat', 'lon', 'km', 'min_km').persist()
 
 
-    new_3 = new_2.withColumn( "city_event", F.when(new_2.min_km == new_2.km, new_2.city) \
-                            .otherwise('NOT')) \
-                            .filter(F.col('city_event') != 'NOT')
+    new_3 = new_2.filter( new_2.km == new_2.min_km).drop('min_km')
 
-    window = Window().partitionBy('message_from', "city_event").orderBy('date')
-    new_4 = new_3.withColumn('days_in', F.dense_rank().over(window))
+    new_4 =  new_3.select('message_from', 'city', 'date').distinct().persist()
 
-    window = Window().partitionBy('message_from', "city_event")
-    new_5 = new_4.withColumn('days_max', F.max('days_in').over(window))
-    new_6 = new_5.withColumn( "city_home", F.when(new_5.days_max > 26 , new_5.city).otherwise('NOT'))
+    window = Window().partitionBy('message_from', "city").orderBy('date')
+    new_5 = new_4.withColumn('rn', F.row_number().over(window))
 
-    window = Window().partitionBy('message_from').orderBy('city_event')
-    new_7 = new_6.withColumn('city_cnt', F.dense_rank().over(window))
+    new_6 =  new_5.withColumn('diff', F.col('date') - F.col('rn'))
+
+    window = Window().partitionBy('message_from', "city", 'diff')
+    new_7 = new_6.withColumn('days_count', F.count('diff').over(window))
+
+    window = Window().partitionBy('message_from').orderBy('city')
+    new_8 = new_7.withColumn('city_count', F.dense_rank().over(window))
 
     window = Window().partitionBy('message_from')
-    new_8 = new_7.withColumn('city_cnt_max', F.max('city_cnt').over(window)). \
-                            select('message_from', 'datetime', 
-                            'date', 'city_event', 
-                             'km', 'days_in', 
-                             'days_max', 'city_home', 
-                             'city_cnt', 'city_cnt_max').persist()
+    new_9 = new_8.withColumn('travel_count', F.max('city_count').over(window))
 
-    dist_geo = new_8.select(F.col('message_from').alias('message_from_2'), 'city_event').distinct()
-    df = (dist_geo.withColumn('lst', F.col('city_event').alias('lst'))
-                             .groupBy('message_from_2')
-                             .agg( F.concat_ws(',', F.collect_list('lst').alias('b_list')).alias('travel_array')))
+    new_10 = new_9.withColumn( "home_city", F.when(new_9.days_count > 26 , new_9.city).otherwise('NOT'))
 
-    join_df = new_8.join(df, new_8.message_from == df.message_from_2, how="inner").drop('message_from_2') \
-                            .select(F.col('message_from').alias('user_id'), 
-                            F.col('city_event').alias('act_city'), 
-                            F.col('city_home').alias('home_city'), 
-                            F.col('city_cnt_max').alias('travel_count'), 
-                            'travel_array', 'datetime')      
+    dist_geo = new_8.select(F.col('message_from').alias('message_from_2'),
+                        F.col('city').alias('city_2'), 
+                        F.col('date').alias('date_2')).orderBy('message_from_2', 'date_2')
 
-    df_with_time =  join_df.withColumn('TIME', F.col('datetime').cast('Timestamp')) \
-                    .withColumn('timezone', F.concat(F.lit('Australia'), F.lit('/'),  F.col('act_city'))) \
-                    .withColumn('local_time', F.from_utc_timestamp(F.col('TIME'), F.col('timezone'))).drop('datetime')
+    df = (dist_geo.withColumn('lst', F.col('city_2').alias('lst'))
+                         .groupBy('message_from_2')
+                         .agg( F.concat_ws(',', F.collect_list('lst').alias('b_list')).alias('travel_array')))
+
+    new_11 =  new_10.join(df, new_8.message_from == df.message_from_2, how="inner") \
+                        .drop('message_from_2', 'days_count', 'city_count', 'rn').persist()
+
+    window = Window().partitionBy('message_from')
+    new_12 = new_11.withColumn('max_date', F.max('date').over(window))        
+
+    new_13 = new_12.withColumn( "act_city", F.when(new_12.max_date == new_12.date, new_5.city).otherwise('NOT')) \
+                                    .drop('city', 'diff', 'max_date')
+
+
+    df_with_time =  new_13.filter(new_13.act_city != "NOT") \
+            .withColumn('TIME', F.col('date').cast('Timestamp')) \
+            .withColumn('timezone', F.concat(F.lit('Australia'), F.lit('/'),  F.col('act_city')))
+            .withColumn('local_time', F.from_utc_timestamp(F.col('TIME'), F.col('timezone'))).drop('date')
+
 
     return df_with_time
 
@@ -111,8 +118,8 @@ def main():
     df_city.write.parquet(destination_path + f'df_city')
 
 
-    calc_user_local_time = df_local_time(df_with_time)
-    calc_user_local_time.write.parquet(destination_path + f'df_local_time')
+    df_local_time = df_local_time(df_with_time)
+    df_local_time.write.parquet(destination_path + f'df_local_time')
 
 
 if __name__ == "__main__":
