5315dd1baa802625ef1a7c77139b89b0b3ecd4b0
22Vadim <105052322+22Vadim@users.noreply.github.com>
2022-11-16 17:23:46 +0300

    Update mart_3.py

--- src/scripts/mart_3.py
+++ src/scripts/mart_3.py
@@ -1,85 +1,95 @@
-import os
-import sys
-import datetime
-
 import findspark
-from scripts.st_2_sliced_by_user import df_local_time
+import sys 
+import os
 findspark.init()
 findspark.find()
 
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
+import pyspark
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.window import Window
+import pyspark.sql.functions as F
+from datetime import datetime
+from pyspark.sql.functions import to_timestamp
 
 os.environ['HADOOP_CONF_DIR'] = '/etc/hadoop/conf'
 os.environ['YARN_CONF_DIR'] = '/etc/hadoop/conf'
 
 
-def df_friends(df_city, geo_cities, df_local_time):
+def df_friends(geo_events_source: str, geo_cities: str, spark: SparkSession) -> DataFrame:
 
-    events = spark.read.parquet(df_city)
+    #подготовка датасетов
+    events = spark.read.parquet(geo_events_source)
 
-    events_day = events.filter(F.col("lat").isNotNull() 
-                           & F.col("lat").isNotNull() & 
-                           (events.event_type == "message"))
+    events_day = events.filter(F.col("lat").isNotNull() & F.col("lat").isNotNull() & 
+                            (events.event_type == "message")).where("date < '2022-03-05'") ####исправить
 
-    geo = spark.read.csv(geo_cities, sep =';', header = True)
+    geo = spark.read.csv(geo_cities, sep =';',header = True)
 
     geo = geo.withColumn('lat', F.regexp_replace('lat', ',', '.').cast('double')) \
-                     .withColumn('lng', F.regexp_replace('lng', ',', '.').cast('double')) \
-                     .select( F.col('city') ,F.col('lat').alias('lat_2'), F.col('lng').alias('lon_2'))
+                        .withColumn('lng', F.regexp_replace('lng', ',', '.').cast('double')) \
+                        .select( F.col('city') ,F.col('lat').alias('lat_2'), F.col('lng').alias('lon_2'))
 
     new = events_day.crossJoin(geo)
 
     new = new.withColumn( 'km' , 2 * 6371 * F.asin(F.sqrt(F.sin(((F.radians(F.col("lat_2"))) - (F.radians(F.col("lat")))) / 2)**2  
-                                                      + F.cos((F.radians(F.col("lat"))))*F.cos((F.radians(F.col("lat_2"))))
-                                                      *F.sin(((F.radians(F.col("lon_2"))) - (F.radians(F.col("lon"))))/2)**2)))
+                                                    + F.cos((F.radians(F.col("lat"))))*F.cos((F.radians(F.col("lat_2"))))
+                                                    *F.sin(((F.radians(F.col("lon_2"))) - (F.radians(F.col("lon"))))/2)**2)))
 
     window = Window().partitionBy('event.message_from', "event.message_id")
+    min_km = new.withColumn("min_km", F.min('km').over(window)).filter(F.col('km') == F.col('min_km'))
 
-    new_2 = new.withColumn("min_km", F.min('km').over(window)).filter(F.col('km') == F.col('min_km')).persist()
+    users = min_km.select('event.message_from', 'event.message_to', 'date', 'city', 'lat', 'lon')\
+                        .filter(F.col('message_to').isNotNull()).dropDuplicates()
 
-    new_3 = new_2.select('event.message_from', 'event.datetime', 'city', 'lat', 'lon').dropDuplicates()
+    #крайний город и правка городов
+    window = Window().partitionBy('message_from').orderBy('date')
+    city_row_num = users.withColumn('row',F.row_number().over(window))
 
-    new_4 = new_3.alias('new_4')
+    window = Window().partitionBy('message_from')
+    city_max = city_row_num.withColumn('max',F.max('row').over(window)).persist()
 
-    new_5 = new_4.select(F.col('message_from').alias('message_from_2'),
-            F.col('datetime').alias('datetime_2'),        
-            F.col('city').alias('city_2'),
+    users_1 = city_max.filter(F.col('row') == F.col('max')).withColumn('zone_id', (F.when((F.col('city') != 'Gold Coast') & (F.col('city') != 'Cranbourne')  
+                        & (F.col('city') != 'Newcastle')
+                        & (F.col('city') != 'Wollongong') & (F.col('city') != 'Geelong') & (F.col('city') != 'Townsville')
+                        & (F.col('city') != 'Ipswich') & (F.col('city') != 'Cairns') & (F.col('city') != 'Toowoomba')
+                        & (F.col('city') != 'Ballarat') & (F.col('city') != 'Bendigo') & (F.col('city') != 'Launceston')
+                        & (F.col('city') != 'Mackay') & (F.col('city') != 'Rockhampton') & (F.col('city') != 'Maitland')
+                        & (F.col('city') != 'Bunbury'), F.col('city')).otherwise('Brisbane'))) \
+                        .withColumn('TIME', to_timestamp(F.col('date'))) \
+                        .withColumn('timezone', F.concat(F.lit('Australia'), F.lit('/'),  F.col('zone_id'))) \
+                        .withColumn('local_time', F.from_utc_timestamp(F.col('TIME'), F.col('timezone'))) \
+                        .drop('row', 'max', 'city', 'TIME', 'date' , 'timezone')                    
+
+    #второй пользователь
+    users_2_d = users_1.alias('users_2')
+
+    users_2 = users_2_d.select(F.col('message_from').alias('message_from_2'), 
             F.col('lat').alias('lat_2'),
             F.col('lon').alias('lon_2'))
 
-    new_6 = new_3.crossJoin(new_5)
+    #соединяем
+    crossJoin_users = users_1.crossJoin(users_2)
 
-    new_7 =  new_6.filter(F.col('message_from') != F.col('message_from_2')).drop('datetime_2')
+    crossJoin_users_clean = crossJoin_users\
+                .filter((F.col('message_from') != F.col('message_from_2')) & (F.col('message_to') != F.col('message_from_2')))
 
-    new_8 = new_7.withColumn( 'km' , 2 * 6371 * F.asin(F.sqrt(F.sin(((F.radians(F.col("lat_2"))) - (F.radians(F.col("lat")))) / 2)**2  
-                                                      + F.cos((F.radians(F.col("lat"))))*F.cos((F.radians(F.col("lat_2"))))
-                                                      *F.sin(((F.radians(F.col("lon_2"))) - (F.radians(F.col("lon"))))/2)**2)))
+    #поиск километра
+    users_km = crossJoin_users_clean.withColumn( 'km' , 2 * 6371 * F.asin(F.sqrt(F.sin(((F.radians(F.col("lat_2"))) - (F.radians(F.col("lat")))) / 2)**2  
+                                                    + F.cos((F.radians(F.col("lat"))))*F.cos((F.radians(F.col("lat_2"))))
+                                                    *F.sin(((F.radians(F.col("lon_2"))) - (F.radians(F.col("lon"))))/2)**2)))
 
-    new_8 = new_8.dropDuplicates()
+    #минимальный километр и финальные столбцы
+    df_users = users_km.filter(F.col('km') < 1 ).withColumn('processed_dttm', F.current_timestamp()) \
+                                    .select(F.col('message_from').alias('user_left'),
+                                        F.col('message_from_2').alias('user_right'),
+                                        'processed_dttm', 'zone_id', 'local_time').dropDuplicates()     
 
-    window = Window().partitionBy('message_from', "message_from_2")
-
-    new_9 = new_8.withColumn("min_km", F.min('km').over(window))
-
-    new_10 = new_9.filter(new_9.km == new_9.min_km).select(F.col('message_from').alias('user_left'),
-                                                      F.col('message_from_2').alias('user_right'),
-                                                      F.col('city').alias('zone_id'), 'km', 'datetime' ).persist()
-
-    new_11 =     new_10.filter(new_10.km <1 ) \
-            .withColumn('processed_dttm', F.current_timestamp()).join(df_local_time, 'city', 'inner')                                                                                                                                                                                          
-
-    df_friends = new_11.select('user_left', 'user_right', 'processed_dttm', 'zone_id', 'local_time')
-    
-    return df_friends
+    return df_users
 
 def main():
-    df_city_path = sys.argv[1]
-    df_local_time_path = sys.argv[2]
-    geo_cities = sys.argv[3]
-    destination_path = sys.argv[4]
-    date = sys.argv[5]
+    geo_events_source = sys.argv[1]
+    geo_cities = sys.argv[2]
+    destination_path = sys.argv[3]
 
     spark = (SparkSession.builder
                         .master('yarn')
@@ -88,16 +98,10 @@ def main():
                         .appName('sliced_by_zones')
                         .getOrCreate())
     
-    df_city = (spark.read.parquet(df_city_path)
-               )
-    df_local_time = (spark.read.parquet(df_local_time_path)
-               )
 
-                   
+    df_friends_d = df_friends(geo_events_source,  geo_cities)
 
-    df_friends = df_friends(df_city, df_local_time)
-
-    df_friends.write.parquet(destination_path + f'df_friends')
+    df_friends_d.write.parquet(destination_path + f'df_friends')
 
 if __name__ == "__main__":
         main()
